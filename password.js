670304ffa9df641c5fef06e373f028962564dd28
jpitech <57502688+jpitech@users.noreply.github.com>
2019-11-07 13:09:55 -0600

    Update password.js

--- password.js
+++ password.js
@@ -8,7 +8,7 @@ while (s != verifyPass()) {
     s = prompt("Please enter your password.");
     if (s == verifyPass()) {
         alert("Access granted. Click OK to continue.")
-        window.location.href = "index.html"; //page to redirect if password entered is correct
+        window.location.href = "home.html"; //page to redirect if password entered is correct
 
     } else {
         alert("Incorrect password-Try again");
