c2e416ea437fff5ae93d73ff9641c52e3f04d12b
jpitech <57502688+jpitech@users.noreply.github.com>
2019-11-07 13:02:49 -0600

    Add files via upload

---
+++ password.js
@@ -0,0 +1,17 @@
+var s = "";
+
+function verifyPass() {
+    return "jpi3146"
+}
+
+while (s != verifyPass()) {
+    s = prompt("Please enter your password.");
+    if (s == verifyPass()) {
+        alert("Access granted. Click OK to continue.")
+        window.location.href = "index.html"; //page to redirect if password entered is correct
+
+    } else {
+        alert("Incorrect password-Try again");
+
+    }
+}
